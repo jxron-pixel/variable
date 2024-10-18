c4d4029685ce0ebedbcc41e7cfe5da7a263ed70b
jxron-pixel <jjspyderx@gmail.com>
2024-10-18 17:39:33 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+x = "Jaron"
+y = "spaceship"
+z = "computer"
+h = 11
+
+print(x)
+print(y)
+print(z)
+print(h)
+
+
+
+favourite_subject =  input("What is your favourite subject?")
+print("hello \n" ,favourite_subject, "\ sin wonderful!")
